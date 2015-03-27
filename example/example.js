0542aeef4f13c919ff0ac546896ce2cb38bd02c2
Nikolay Bekirov <nbekirov@gmail.com>
2015-03-27 15:26:14 +0200

    Improve example page

--- example/example.js
+++ example/example.js
@@ -7,10 +7,24 @@ requirejs.config({
 
 requirejs(['jquery', 'rpn/main'], function ($, rpn) {
     $('#transfrom_infix_button').bind('click', function () {
-        $('#rpn_input').val(rpn.getFromInfix($('#infix_input').val()));
+        var result = '';
+        try {
+            result = rpn.getFromInfix($('#infix_input').val())
+        } catch (err) {
+            alert(err);
+        }
+
+        $('#rpn_input').val(result);
     });
 
     $('#solve_rpn_button').bind('click', function () {
-        $('#result_output').val(rpn.solve($('#rpn_input').val()));
+        var result = '';
+        try {
+            result = rpn.solve($('#rpn_input').val())
+        } catch (err) {
+            alert(err);
+        }
+
+        $('#result_output').val(result);
     });
 });
