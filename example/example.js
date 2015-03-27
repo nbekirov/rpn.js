baf36f125edf44c28f331e266f7887dff5b0456c
Nikolay Bekirov <nbekirov@gmail.com>
2015-03-27 15:16:34 +0200

    (Re)Structure project

--- example/example.js
+++ example/example.js
@@ -1,9 +1,16 @@
-define(['jquery', 'app/rpn'], function ($, rpn) {
+requirejs.config({
+    "baseUrl": "../lib",
+    "paths": {
+        "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min"
+    }
+});
+
+requirejs(['jquery', 'rpn/main'], function ($, rpn) {
     $('#transfrom_infix_button').bind('click', function () {
         $('#rpn_input').val(rpn.getFromInfix($('#infix_input').val()));
-    })
+    });
 
     $('#solve_rpn_button').bind('click', function () {
         $('#result_output').val(rpn.solve($('#rpn_input').val()));
-    })
+    });
 });
--- js/app.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-    "baseUrl": "js/lib",
-    "paths": {
-        "app": "../app",
-        "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min"
-    }
-});
-
-requirejs(["app/main"]);
