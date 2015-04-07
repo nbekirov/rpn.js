80a1bd5d723a6078fc576005df07e795e8a93676
Nikolay Bekirov <nbekirov@gmail.com>
2015-04-07 16:06:36 +0300

    Add Tokenizer tests

--- tests/rpn/tokenizer_test.spec.js
+++ tests/rpn/tokenizer_test.spec.js
@@ -1,6 +1,42 @@
 define(['rpn/tokenizer'], function (tokenizer) {
     'use strict';
 
-    describe('Tokenizer tests', function () {
+    describe('Tokenizer', function () {
+        it("should ignore empty strings", function () {
+            expect(tokenizer('')).toEqual([]);
+            expect(tokenizer('    ')).toEqual([]);
+        });
+
+        it("should ignore whitespace", function () {
+            expect(tokenizer('   1  2    ')).toEqual(['1', '2']);
+        });
+
+        it("should ignore non-operators", function () {
+            expect(tokenizer('1noop 2 noop3|4')).toEqual(['1', '2', '3', '4']);
+        });
+
+        it("should detect operators", function () {
+            expect(tokenizer('+-*x/%pow_')).toEqual(['+', '-', '*', 'x', '/', '%', 'pow', '_']);
+        });
+
+        it("should detect mixed-case operators and return lowercase", function () {
+            expect(tokenizer('X PoW POW pOw')).toEqual(['x', 'pow', 'pow', 'pow']);
+        });
+
+        it("should detect parentheses", function () {
+            expect(tokenizer('()')).toEqual(['(', ')']);
+        });
+
+        it("should detect floats", function () {
+            expect(tokenizer('0.500000000')).toEqual(['0.500000000']);
+        });
+
+        it("should NOT detect negative", function () {
+            expect(tokenizer('-3')).toEqual(['-', '3']);
+        });
+
+        it("should detect mix of numbers operators and  parentheses", function () {
+            expect(tokenizer('0.500000000+ -(-3/-2)')).toEqual(['0.500000000', '+', '-', '(', '-', '3', '/', '-', '2', ')']);
+        });
     });
 });
