6c9855d2416bba32b7a110eae42ebe04fa30c7bc
Nikolay Bekirov <nbekirov@gmail.com>
2015-04-08 14:40:30 +0300

    Add RPN eval tests

--- tests/rpn/rpn_eval_test.spec.js
+++ tests/rpn/rpn_eval_test.spec.js
@@ -1,6 +1,69 @@
 define(['rpn/main'], function (rpn) {
     'use strict';
 
-    describe('RPN eval tests', function () {
+    describe('RPN eval', function () {
+        it("should throw on empty expressions", function () {
+            function testThrow() {
+                return rpn.evaluate('');
+            }
+
+            expect(testThrow).toThrowError(/Invalid expression/);
+        });
+
+        it("should NOT transform single-number non-negative expressions", function () {
+            expect(rpn.evaluate('2')).toEqual(2);
+            expect(rpn.evaluate('2.5')).toEqual(2.5);
+        });
+
+        it("should throw on insufficient operands for binary operators", function () {
+            function testThrow(arg) {
+                rpn.evaluate(arg);
+            }
+
+            expect(testThrow.bind(null, '2 +')).toThrowError(/Not enough operands in stack/);
+            expect(testThrow.bind(null, '3 -')).toThrowError(/Not enough operands in stack/);
+            expect(testThrow.bind(null, '3 *')).toThrowError(/Not enough operands in stack/);
+            expect(testThrow.bind(null, '3 x')).toThrowError(/Not enough operands in stack/);
+            expect(testThrow.bind(null, '3 /')).toThrowError(/Not enough operands in stack/);
+            expect(testThrow.bind(null, '3 %')).toThrowError(/Not enough operands in stack/);
+            expect(testThrow.bind(null, '3 pow')).toThrowError(/Not enough operands in stack/);
+        });
+
+        it("should throw on insufficient operands for unary operators", function () {
+            function testThrow() {
+                rpn.evaluate('_');
+            }
+
+            expect(testThrow).toThrowError(/Not enough operands in stack/);
+        });
+
+        it("should throw on too many operands for given operators", function () {
+            function testThrow(arg) {
+                rpn.evaluate(arg);
+            }
+
+            expect(testThrow.bind(null, '3 4 5 +')).toThrowError(/Invalid expression/);
+            expect(testThrow.bind(null, '3 4 5 -')).toThrowError(/Invalid expression/);
+            expect(testThrow.bind(null, '3 4 5 *')).toThrowError(/Invalid expression/);
+            expect(testThrow.bind(null, '3 4 5 x')).toThrowError(/Invalid expression/);
+            expect(testThrow.bind(null, '3 4 5 /')).toThrowError(/Invalid expression/);
+            expect(testThrow.bind(null, '3 4 5 %')).toThrowError(/Invalid expression/);
+            expect(testThrow.bind(null, '3 4 5 pow')).toThrowError(/Invalid expression/);
+            expect(testThrow.bind(null, '3 4 _')).toThrowError(/Invalid expression/);
+        });
+
+        it("should get the right answer for binary operators", function () {
+            expect(rpn.evaluate('2 3 +')).toEqual(5);
+            expect(rpn.evaluate('2 3 -')).toEqual(-1);
+            expect(rpn.evaluate('2 3 *')).toEqual(6);
+            expect(rpn.evaluate('2 3 x')).toEqual(6);
+            expect(rpn.evaluate('2 3 /')).toBeCloseTo(0.6666);
+            expect(rpn.evaluate('2 3 %')).toEqual(2);
+            expect(rpn.evaluate('2 3 pow')).toEqual(8);
+        });
+
+        it("should get the right answer for unary operators", function () {
+            expect(rpn.evaluate('2 _')).toEqual(-2);
+        });
     });
 });
